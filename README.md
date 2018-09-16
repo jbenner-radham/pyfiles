7ed4d23bd2c37fbe85d69e8b06d9a314e8a357c6
James Benner <james.benner@gmail.com>
2018-09-15 22:21:03 -0400

    Added some stuff... yay!

---
+++ README.md
@@ -0,0 +1,14 @@
+pyfiles
+=======
+> Just playing around with various implementations of collecting a filtered
+> list of files.
+
+Install
+-------
+```sh
+$ pipenv install
+```
+
+License
+-------
+The MIT License (Expat). See the [license file](LICENSE) for details.
---
+++ pyfiles/__init__.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import List
+from functional import seq
+
+EXCLUDES = ['.git', '.vscode']
+
+def in_excludes(path: Path) -> bool:
+    return any([part in EXCLUDES for part in path.parts])
+
+
+def implementation_0() -> List[Path]:
+    root = Path('.')
+    paths = root.glob('**/*')
+
+    return [path for path in paths
+            if not in_excludes(path) and path.is_file()]
+
+
+def implementation_1() -> List[Path]:
+    root = Path('.')
+    paths = root.glob('**/*')
+
+    def is_file(path: Path) -> bool:
+        return path.is_file()
+
+    return seq(paths).filter_not(in_excludes)\
+                     .filter(is_file)\
+                     .to_list()
